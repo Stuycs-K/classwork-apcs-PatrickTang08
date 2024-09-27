70bce502978d5cc9e5659de22cf491672b9ced48
Patrick Tang <patrickt74@nycstudents.net>
2024-09-27 11:47:50 -0400

    Added preliminary mehtods for return copy and concatArray

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,6 +1,8 @@
 public class MyArrays{
   public static void main(String[] args){
     System.out.println(arrayToString(new int[]{1, 2, 3, 4, 5}));
+    System.out.println(arrayToString(returnCopy(new int[5])));
+    System.out.println(arrayToString(concatArray(new int[5], new int[4])));
   }
   public static String arrayToString(int[] nums){
     String s="[";
@@ -11,5 +13,16 @@ public class MyArrays{
       }
     }
     return s+"]";
+  }
+  public static int[] returnCopy(int[]ary){
+    int[] ans = new int[ary.length];
+    for(int i=0; i<ary.length;i++){
+      ans[i]=ary[i];
+    }
+    return ans;
+  }
+  public static int[] concatArray(int[]ary1,int[]ary2){
+    int[] result = new int[ary1.length+ary2.length];
+    return result;
   }
 }
