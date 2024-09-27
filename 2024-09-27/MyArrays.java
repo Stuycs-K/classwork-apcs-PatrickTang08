82442afd47baf927ffb55f1e32156d76a44fe5da
Patrick Tang <patrickt74@nycstudents.net>
2024-09-27 11:36:55 -0400

    Added arrayToString and main

---
+++ 2024-09-27/MyArrays.java
@@ -0,0 +1,15 @@
+public class MyArrays{
+  public static void main(String[] args){
+    System.out.println(arrayToString(new int[]{1, 2, 3, 4, 5}));
+  }
+  public static String arrayToString(int[] nums){
+    String s="[";
+    for(int i=-0; i<nums.length;i++){
+      s=s+nums[i];
+      if(i<nums.length-1){
+        s+=", ";
+      }
+    }
+    return s+"]";
+  }
+}
