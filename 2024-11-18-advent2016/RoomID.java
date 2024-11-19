2bb7d812d3e4c5eb7f71c1bd01fb2e906bb9fb9f
Patrick Tang <patrickt74@nycstudents.net>
2024-11-19 17:02:16 -0500

    Read all code correctly and got code and id

--- 2024-11-18-advent2016/RoomID.java
+++ 2024-11-18-advent2016/RoomID.java
@@ -2,21 +2,28 @@ import java.io.*;
 import java.util.*;
 
 
-public class Keypad{
+public class RoomID{
   public static void main(String[] args){
-    System.out.println(passcode("Input.txt"));
+    System.out.println(idsum("Input.txt"));
   }
-  public static int passcode(String filename){
+  public static int idsum(String filename){
     int out = 0;
-    try{
-      File file = new File(filename);
-      Scanner sc= new Scanner(file);
-      }
-    }catch (Exception ex)
-    {
+      try{
+        File file = new File(filename);
+        Scanner sc= new Scanner(file);
+        for(int i=0;sc.hasNextLine();i++){
+          String line = sc.nextLine();
+          String[] a = line.split("-");
+          String code=a[a.length-1].substring(4,9);
+          int id= Integer.parseInt(a[a.length-1].substring(0,3));
+          System.out.println(code);
+          System.out.println(id);
+          System.out.println(Arrays.toString(a));
+        }
+        return 0;
+      }catch (Exception ex){
       System.out.println("Exception");
       return 0;
     }
-    return out;
   }
 }
