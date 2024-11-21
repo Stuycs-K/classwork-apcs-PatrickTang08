f6c3b95057ae8b744c1880004f08f28bb5a4cc96
Patrick Tang <patrickt74@nycstudents.net>
2024-11-20 21:54:16 -0500

    Able to read and seperate all info correctly

--- 2024-11-18-advent2016/RoomID.java
+++ 2024-11-18-advent2016/RoomID.java
@@ -13,18 +13,23 @@ public class RoomID{
         Scanner sc= new Scanner(file);
         for(int i=0;sc.hasNextLine();i++){
           String line = sc.nextLine();
-          String[] a = line.split("-");
-          String code=a[a.length-1].substring(4,9);
-          int id= Integer.parseInt(a[a.length-1].substring(0,3));
+          String code=line.substring(line.length()-6,line.length()-1);
+          int id= Integer.parseInt(line.substring(line.length()-10,line.length()-7));
           String[] alphabet = new String[]{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
           int[] count =new int[26];
-          for(int j=0; i<alphabet.length;j++){
-            icount[j]=count[j]+1;
+          String name= line.substring(0,line.length()-11);
+          for(int j=0;j<name.length();j++){
+            for(int k=0;k<alphabet.length;k++){
+              if(name.substring(j,j+1).equals(alphabet[k])){
+                count[k]=count[k]+1;
+              }
+            }
           }
-          System.out.println(Arrays.toString(alphabet));
+          System.out.println(Arrays.toString(count));
+          System.out.println(name);
           System.out.println(code);
           System.out.println(id);
-          System.out.println(Arrays.toString(a));
+          System.out.println(line);
         }
         return 0;
       }catch (Exception ex){
