e567ba3107bdab538ff4fb33e8be0b1cd587da51
Patrick Tang <patrickt74@nycstudents.net>
2024-11-21 00:35:17 -0500

    Finished code

--- 2024-11-18-advent2016/Taxicab.java
+++ 2024-11-18-advent2016/Taxicab.java
@@ -3,28 +3,27 @@ import java.util.*;
 
 public class Taxicab{
   public static void main(String[] args){
-    String[]data= parse("Input.txt");
-    System.out.println(parse("Input.txt"));
+    System.out.println(distance("Input.txt"));
   }
-  public static String[] parse(String filename){
+  public static int distance(String filename){
     try{
       File file = new File(filename);
       Scanner sc= new Scanner(file);
       String line = sc.nextLine();
-      return line.split(", ");
-    }catch (Exception e)
-    {
+      String[] move=line.split(", ");
+      int dir=0;
+      int[] step=new int[4];
+      for(int i=0;i<move.length;i++){
+        String turn = move[i].substring(0,1);
+        if(turn.equals("R")){
+          dir = (dir + 1) % 4;
+        }else dir = (dir + 3) % 4;
+        step[dir]=step[dir]+Integer.parseInt(move[i].substring(1));
+      }
+      return step[0]-step[2]+step[1]-step[3];
+    }catch (Exception e){
       System.out.println("Exception");
-      String[]a=new String[1];
-      return a;
+      return 0;
     }
-  }
-  public static int distance(String[]data){
-    int x = 0, y = 0, dir = 0;
-    for(int i=0; i < data.length; i++){
-      String heading = data[i].substring(0,1);
-      int dis = Integer.parseInt(data[i].substring(1,2));
-    }
-    return 0;
   }
 }
