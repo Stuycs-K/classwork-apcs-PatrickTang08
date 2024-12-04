67623a2065721429a622147149dc144c614fe957
Patrick Tang <patrickt74@nycstudents.net>
2024-12-04 09:41:34 -0500

    Added colors to text

---
+++ 2024-12-03-colors/ColorDemo.java
@@ -0,0 +1,25 @@
+public class ColorDemo{
+  public static void main(String[] args) {
+    System.out.print("This ");
+    System.out.print("\u001b[31m");
+    System.out.print("is ");
+    System.out.print("\u001b[32m");
+    System.out.print("the ");
+    System.out.print("\u001b[33m");
+    System.out.print("extent ");
+    System.out.print("\u001b[34m");
+    System.out.print("of ");
+    System.out.print("\u001b[35m");
+    System.out.print("my ");
+    System.out.print("\u001b[36m");
+    System.out.print("creative ");
+    System.out.print("\u001b[37m");
+    System.out.print("ability. ");
+    System.out.print("\u001b[30m");
+    System.out.print("Oh u can't see this.");
+    System.out.print("\u001b[38;2;87;141;189m");
+    System.out.print("Random RGB. ");
+    System.out.print("\u001b[0m");
+    System.out.print("Reset");
+  }
+}
