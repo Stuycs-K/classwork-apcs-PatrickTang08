dc1b5669721604a46799ccdab61d14c644195706
Patrick Tang <patrickt74@nycstudents.net>
2024-12-04 22:07:15 -0500

    Added first 3 numbers and colors for them

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,13 +1,28 @@
+import java.util.Random;
+
 public class Driver{
   public static void main(String[] args) {
     System.out.print(Text.CLEAR_SCREEN);
     System.out.print(Text.HIDE_CURSOR);
+    Text.color(Text.BLUE, Text.background(Text.BLUE));
     drawBorder(80,30);
-    Text.go(31, 1);
+    int[] nums=new int[3];
+    Random rng= new Random();
+    for(int i=0;i<3;i++){
+        System.out.println(Text.RESET);
+        nums[i] = rng.nextInt(100);
+        Text.go(2,i*20+20);
+        if(nums[i]>75){
+          Text.color(Text.GREEN,Text.BRIGHT);
+        }else if(nums[i]<25){
+          Text.color(Text.RED,Text.BRIGHT);
+        }else Text.color(Text.WHITE);
+        System.out.print(nums[i]);
+    }
     System.out.println(Text.RESET);
+    Text.go(31, 1);
   }
   public static void drawBorder(int width, int height) {
-    Text.color(Text.BLUE, Text.background(Text.BLUE));
     for(int col= 1;col<=width;col++) {
         Text.go(1,col);
         System.out.print("o");
