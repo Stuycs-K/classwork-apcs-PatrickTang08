f17b60acba1a0f9738a523466611e177153f1174
Patrick Tang <patrickt74@nycstudents.net>
2024-12-06 11:34:08 -0500

    Added barbarian class and all methods

---
+++ 2024-12-06-Adventurer/Barbarian.java
@@ -0,0 +1,59 @@
+public class Barbarian extends Adventurer{
+  private String name;
+  private int HP,maxHP;
+  private int rage,maxRage;
+
+  public Barbarian(String name){
+    super(name);
+  }
+  public Barbarian(String name, int hp){
+    super(name,hp);
+  }
+
+  /*
+    all adventurers must have a custom special
+    consumable resource (mana/rage/money/witts etc)
+  */
+  //give it a short name (fewer than 13 characters)
+  public String getSpecialName(){
+    return "Barbarian Barrel";
+  }
+  public int getSpecial(){
+    return rage;
+  }
+  public void setSpecial(int n){
+    if(n>this.getSpecialMax()){
+      this.rage=getSpecialMax();
+    }else this.rage=n;
+  }
+  public int getSpecialMax(){
+    return maxRage;
+  }
+  /*
+    all adventurers must have a way to attack enemies and
+    support their allys
+  */
+  //hurt or hinder the target adventurer
+  public String attack(Adventurer other){
+    other.setHP(other.getHP()-3);
+    return this.getName()+ " stabs "+other.getName()+".";
+  }
+
+  //heall or buff the target adventurer
+  public String support(Adventurer other){
+    other.setHP(other.getHP()+1);
+    return this.getName()+ " heals "+other.getName()+".";
+  }
+
+  //heall or buff self
+  public String support(){
+    this.setHP(this.getHP()+1);
+    return this.getName()+ " heals "+this.getName()+".";
+  }
+
+  //hurt or hinder the target adventurer, consume some special resource
+  public String specialAttack(Adventurer other){
+    other.setHP(other.getHP()-5);
+    return this.getName()+ " rolls on "+other.getName()+".";
+  }
+}
