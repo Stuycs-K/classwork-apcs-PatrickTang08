adc4b9bd96ee37db1194ce6a0b955a3c0fe812fa
Patrick Tang <patrickt74@nycstudents.net>
2025-01-03 11:38:32 -0500

    Added makeWords

--- 2025-01-02/Fibonacci.java
+++ 2025-01-02/Fibonacci.java
@@ -1,11 +1,19 @@
 public class Fibonacci{
   public static void main(String[] args) {
-    int num=Integer.parseInt(args[0]);
-    System.out.println(fib(num));
+    makeWords(10,"","01");
   }
   public static int fib(int n){
     if(n>1){
       return fib(n-1)+fib(n-2);
     }else return n;
+  }
+  public static void makeWords(int remainingLetters, String result, String alphabet){
+    if(remainingLetters>0){
+      for(int i=0;i<alphabet.length();i++){
+        makeWords(remainingLetters-1,result+alphabet.substring(i,i+1),alphabet);
+    }
+    }else{
+      System.out.println(result);
+    }
   }
 }
